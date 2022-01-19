5d39898a84fef13bb526d6d129d0afa39fd1b7c2
Raden Darajat Bambang Eka Gunawan <send.raden@gmail.com>
2022-01-19 12:05:10 +0800

    Read and Write

--- PenulisStream_BesiBerani/Program.cs
+++ PenulisStream_BesiBerani/Program.cs
@@ -13,6 +13,34 @@ namespace PenulisStream_BesiBerani
 
             f.Blobbo(f.Blobbo(f.Blobbo(aax),aax), aax);
 
-        }
-    }
-}
+            var volder = Environment.GetFolderPath(Environment.SpecialFolder.Personal);
+
+            string the_address = f.full_path;
+            string destination = Flobbo.gen_address;
+
+            //var pembaca = new StreamReader($"{volder}{Path.DirectorySeparatorChar}{the_address}");
+            //var penulis = new StreamWriter($"{volder}{Path.DirectorySeparatorChar}{destination}mailToCaptainA.txt");
+            var pembaca = new StreamReader($"{the_address}");
+            var penulis = new StreamWriter($"{destination}mailToCaptainA.txt");
+
+            penulis.WriteLine("To: CaptainAmazing@objectville.net");
+            penulis.WriteLine("From: Commissioner@objectville.net");
+            penulis.WriteLine("Subject: Can you save the day... again?");
+            penulis.WriteLine();
+            penulis.WriteLine("We’ve discovered the Swindler’s terrible plan:");
+
+            while (!pembaca.EndOfStream) {
+                var lineFromThePlan = pembaca.ReadLine();
+                penulis.WriteLine($"The plan -> {lineFromThePlan}");
+            }
+
+            penulis.WriteLine();
+            penulis.WriteLine("Can you help us?");
+
+            penulis.Close();
+            pembaca.Close();
+
+
+        } //end Main
+    }//end class
+} // end namespace
