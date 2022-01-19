3d8dcce2fd732ae3e70c56ee9278f7c9932e20ac
Raden Darajat Bambang Eka Gunawan <send.raden@gmail.com>
2022-01-19 11:00:13 +0800

    Add project files.

---
+++ PenulisStream_BesiBerani/Flobbo.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace PenulisStream_BesiBerani
+{
+    class Flobbo
+    {
+        //field(S)
+        private string zap;
+
+        // Constructor
+        public Flobbo(string zap)
+        {
+            this.zap = zap;
+        }
+
+        //Method #1
+        public StreamWriter Snobbo()
+        {
+            return new StreamWriter("C:\\Users\\sendr\\Documents\\Experiment_Result\\macaw.txt");
+        } // end Method #1
+
+        //Method #2
+        public bool Blobbo(StreamWriter jcz)
+        {
+            jcz.WriteLine(this.zap);
+            this.zap = "green purple";
+            return false;
+        }// end Method # 2
+
+        // Method #3
+        public bool Blobbo (bool Already, StreamWriter sw)
+        {
+            if (Already)
+            {
+                sw.WriteLine(this.zap);
+                sw.Close();
+                return false;
+            } else
+            {
+                sw.WriteLine(this.zap);
+                this.zap = "red orange";
+                return true;
+            }
+
+                
+        }
+        // end Method #3
+
+
+
+
+
+
+
+
+    } //end class
+} //end namespace
